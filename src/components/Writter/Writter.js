21c14211e9ae63e761b5ad1e82fc597a6d3ecd06
AbdullahWins <noyononline@gmail.com>
2023-04-17 15:38:39 +0600

    added images and new section

--- src/components/Writter/Writter.js
+++ src/components/Writter/Writter.js
@@ -1,4 +1,6 @@
 import React from "react";
+import icon1 from "../../assets/icons/icon1.svg";
+import icon2 from "../../assets/icons/icon2.svg";
 
 const Writter = () => {
   return (
@@ -11,7 +13,33 @@ const Writter = () => {
           Please do not write a chatGPT prompt here. Our system will do that for
           you.
         </p>
-        <div className="pb-24"></div>
+        <sectionn className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start justify-center p-4">
+          <div className="grid grid-cols-12 gap-1 items-start justify-center h-48 border-2 border-titleGradientStart rounded-2xl p-4">
+            <img className="col-span-3" src={icon1} alt="" />
+            <div className="col-span-9 text-start">
+              <p className="text-xl font-bold">Write a Press Release</p>
+              <p className="text-base">
+                Write a description of your news announcement in the Step 2
+                form. Remember, be conversational. Pretend that you're
+                describing your news to a friend. You really should not need
+                more than about 100 words. Refer to the Ideas for tips.
+              </p>
+            </div>
+          </div>
+          <div className="grid grid-cols-12 gap-1 items-start justify-center h-48 border-2 border-titleGradientStart rounded-2xl p-4">
+            <img className="col-span-3" src={icon2} alt="" />
+            <div className="col-span-9 text-start">
+              <p className="text-xl font-bold">
+                Improve My Existing Press Release
+              </p>
+              <p className="text-base">
+                Paste your existing press release in the Step 2 form. For
+                existing releases, Newswriter always returns creative ideas that
+                help you improve your already written press release.
+              </p>
+            </div>
+          </div>
+        </sectionn>
       </div>
     </section>
   );
