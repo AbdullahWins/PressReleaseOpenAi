669d006d4df7951649a8fc4c6f4ddbe56e31afe6
AbdullahWins <noyononline@gmail.com>
2023-04-17 13:40:13 +0600

    added custom tailwind theme and topnav routes

--- src/components/shared/TopNav/TopNav.js
+++ src/components/shared/TopNav/TopNav.js
@@ -1,23 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const TopNav = () => {
   return (
-    <section className="flex flex-col md:flex-row items-center justify-between gap-4 bg-white md:rounded-full px-4 md:px-20 py-3">
-      <button className="text-xl btn btn-ghost hover:bg-purple-600 hover:text-white rounded-full normal-case">
-        Help
-      </button>
-      <button className="text-xl btn btn-ghost hover:bg-purple-600 hover:text-white rounded-full normal-case">
-        About Us
-      </button>
-      <button className="text-xl btn btn-ghost hover:bg-purple-600 hover:text-white rounded-full normal-case">
-        Home
-      </button>
-      <button className="text-xl btn btn-ghost hover:bg-purple-600 hover:text-white rounded-full normal-case">
-        Support
-      </button>
-      <button className="text-xl btn btn-ghost hover:bg-purple-600 hover:text-white rounded-full normal-case">
-        Privacy
-      </button>
+    <section className="flex flex-col md:flex-row items-center justify-between gap-4 bg-whiteHigh md:rounded-full px-4 md:px-20 py-3">
+      <Link to="/help">
+        <button className="text-xl btn btn-ghost hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case">
+          Help
+        </button>
+      </Link>
+      <Link to="/about">
+        <button className="text-xl btn btn-ghost hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case">
+          About us
+        </button>
+      </Link>
+      <Link to="/home">
+        <button className="text-xl btn btn-ghost hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case">
+          Home
+        </button>
+      </Link>
+      <Link to="/support">
+        <button className="text-xl btn btn-ghost hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case">
+          Support
+        </button>
+      </Link>
+      <Link to="/privacy">
+        <button className="text-xl btn btn-ghost hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case">
+          Privacy
+        </button>
+      </Link>
     </section>
   );
 };
