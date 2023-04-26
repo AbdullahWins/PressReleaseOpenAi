bce31d9bd0236fd11823c0ffbf4a3b189cb36272
Abdullah Al MahMud <abdudevs@gmail.com>
2023-04-26 13:20:54 +0600

    added dynamic color change on select

--- src/components/shared/TopNav/TopNav.js
+++ src/components/shared/TopNav/TopNav.js
@@ -3,32 +3,64 @@ import { Link } from "react-router-dom";
 import TopNavResponsive from "./TopNavResponsive";
 
 const TopNav = () => {
+  const pageTitle = document.title;
+
   return (
     <div>
       <TopNavResponsive></TopNavResponsive>
       <section className="hidden md:flex items-center justify-between gap-4 bg-whiteHigh rounded-full px-4 md:px-20 py-3">
         <Link to="/help" className="">
-          <button className="text-xl btn btn-ghost hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case">
+          <button
+            className={`text-xl btn btn-ghost ${
+              pageTitle === `${process.env.REACT_APP_ApplicationName} | Help`
+                ? "bg-btnHoverColor text-whiteHigh"
+                : ""
+            } hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case`}
+          >
             Help
           </button>
         </Link>
         <Link to="/about">
-          <button className="text-xl btn btn-ghost hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case">
+          <button
+            className={`text-xl btn btn-ghost ${
+              pageTitle === `${process.env.REACT_APP_ApplicationName} | About`
+                ? "bg-btnHoverColor text-whiteHigh"
+                : ""
+            } hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case`}
+          >
             About us
           </button>
         </Link>
         <Link to="/home">
-          <button className="text-xl btn btn-ghost hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case">
+          <button
+            className={`text-xl btn btn-ghost ${
+              pageTitle === `${process.env.REACT_APP_ApplicationName} | Home`
+                ? "bg-btnHoverColor text-whiteHigh"
+                : ""
+            } hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case`}
+          >
             Home
           </button>
         </Link>
         <Link to="/support">
-          <button className="text-xl btn btn-ghost hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case">
+          <button
+            className={`text-xl btn btn-ghost ${
+              pageTitle === `${process.env.REACT_APP_ApplicationName} | Support`
+                ? "bg-btnHoverColor text-whiteHigh"
+                : ""
+            } hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case`}
+          >
             Support
           </button>
         </Link>
         <Link to="/privacy" className="">
-          <button className="text-xl btn btn-ghost hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case">
+          <button
+            className={`text-xl btn btn-ghost ${
+              pageTitle === `${process.env.REACT_APP_ApplicationName} | Privacy`
+                ? "bg-btnHoverColor text-whiteHigh"
+                : ""
+            } hover:bg-btnHoverColor hover:text-whiteHigh rounded-full normal-case`}
+          >
             Privacy
           </button>
         </Link>
